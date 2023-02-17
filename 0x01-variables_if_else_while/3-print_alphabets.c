dc34a0f9d50ebc99a11cd2a20cdad96bc6be438b
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-17 12:12:57 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch, CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+
+	return (0);
+}
