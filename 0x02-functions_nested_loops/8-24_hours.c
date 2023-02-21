d0a852a5939a4bceb553858703d3b7250eb588ac
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-21 04:36:09 -0800

    every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer -entery
+ *
+ * description: prints every minute of the day
+ *
+ * Return: always void
+ */
+void jack_bauer(void)
+	{
+		int a, b, c, d;
+
+		a = 0;
+		while (a < 3)
+		{
+			b = 0;
+			while (b <= 9)
+			{
+				if (a == 2 && b > 3)
+				{
+					break;
+				}
+				c = 0;
+				while (c <= 5)
+				{
+					d = 0;
+					while (d <= 9)
+					{
+						_putchar('0' + a);
+						_putchar('0' + b);
+						_putchar(':');
+						_putchar('0' + c);
+						_putchar('0' + d);
+						_putchar('\n')
+					}
+				}
+			}
+		}
+	}
+
