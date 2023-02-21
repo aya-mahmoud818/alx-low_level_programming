79f4eb31c826773a4d2fb408d321cf81fcc709ba
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-21 12:53:54 -0800

    sdasd

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function prints alphabets
+ *
+ * description: prints alphabets from a-z
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int sum = 0;
+	
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 5 == 0 || n % 3 == 0)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
