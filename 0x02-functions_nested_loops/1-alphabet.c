03e8a0085b538bcec7536577b966ca6ab26943bf
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-21 01:05:34 -0800

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -entery
+ *
+ * description: print_alphabet
+ *
+ * Return: always void
+ */
+void print_alphabet(void)
+	{
+		char x;
+
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+			_putchar('\n');
+	}
