282d77296d58d2a52c09e5ad5bf71c8749ada347
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-21 13:37:32 -0800

    asdadsa

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - function prints alphabets
+ *
+ * description: prints alphabets from a-z
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	unsigned long prev = 1, prev2 = 2, temp;
+
+	printf("%d, ", 1);
+	printf("%d, ", 2);
+	for (n = 3; n <= 98; n++)
+	{
+		temp = prev;
+		prev = prev2;
+		prev2 = temp + prev2;
+		if (n == 98)
+		{
+			printf("%lu\n", prev2);
+		} else
+		{
+			printf("%lu, ", prev2);
+		}
+	}
+	return (0);
+
+}
