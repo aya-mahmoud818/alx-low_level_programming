5e879a4a115de66cbcd8185f83dac8867fed1e7c
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-27 07:09:30 -0800

    str length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen -entery
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '/0')
+	{
+		j++;
+	}
+
+	_putchar(j)
+}
