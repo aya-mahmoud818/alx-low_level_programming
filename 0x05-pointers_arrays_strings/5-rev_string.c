f7fba07914a93607a0aa82088e5bcbf0061cfd47
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-27 09:25:28 -0800

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string -entery
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0, left = 0, char temp, int right;
+
+	while (s[length])
+	{
+		length++;
+	}
+	while (left <= length / 2)
+	{
+		right = length - 1 - left;
+		temp = s[right];
+		s[right] = s[left];
+		s[left] = temp;
+		left++;
+	}
+}
