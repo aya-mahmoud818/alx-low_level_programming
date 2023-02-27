9867aacd3bcbfcfc89449350159b6529274269a1
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-02-27 10:26:45 -0800

    asdasd

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array -entery
+ *
+ * @a: string
+ * @n: number
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", a[x]);
+		if (x != n - 2)
+		{
+			printf(", ");
+		}
+	}
+	printf('\n');
+}
