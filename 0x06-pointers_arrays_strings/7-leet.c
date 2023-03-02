e9f97602aae864408eee1d9e6b5f0839bd9a3c70
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-03-02 05:34:48 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * *leet -entery
  *
@@ -8,17 +9,17 @@
  */
 char *leet(char *s)
 {
-	int i = 0;
-	char letters = "aAeEoOtTlL";
-	char numbers = "4433007711";
+	int i = 0, j;
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
 
 	while (s[i])
 	{
-		for (int j = 0; letters[j]; j++)
+		for (j = 0; letters[j]; j++)
 		{
 			if (s[i] == letters[j])
 			{
-				s[i] == numbers[j];
+				s[i] = numbers[j];
 			}
 		}
 		i++;
