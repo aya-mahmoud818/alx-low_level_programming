fb6c70480dfd0448c04f2c7a5cd6dc6facd236a4
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-03-06 08:17:00 -0800

    Asdasd

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *_strstr -entery
+ *
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, start;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			start = i;
+			for (j = 0; needle[j] == '\0' || haystack[i] == needle [j]; j++)
+			{
+				if (needle[j] == '\0')
+				{
+					return start;
+				}
+				i++;
+			}
+		}
+	}
+	return (0);
+}
+
