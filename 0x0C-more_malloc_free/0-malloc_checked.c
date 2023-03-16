4b9cbd725acc71a2facd42556bccf27e1d90b22c
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-03-16 06:31:35 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,20 +3,22 @@
 /**
  * *malloc_checked -entery
  *
- * @b: unsigned int to bbe checked
+ * @b: size of memory
  *
  * Return: 0
  */
 void *malloc_checked(unsigned int b)
 {
-	*b = sizeof(unsigned int);
+	int s;
 
-	if (*b == NULL)
+	*s = malloc(b);
+
+	if (*s == NULL)
 	{
 		exit(98);
 	}
 	else
 	{
-		return (b);
+		return (s);
 	}
 }
