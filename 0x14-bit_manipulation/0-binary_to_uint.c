caaf49e7312ac849f4b4ce52d8d0f6eabee7b08f
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-04-09 07:55:17 -0700

    binary to unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_uint -entery
+ *
+ * @b: pointer
+ *
+ * Return: value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int i = 0;
+	int base = 1;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		value = value + ((b[i] - 48) * base);
+		base = base * 2;
+	}
+	return (value);
+}
